77978287dd7fceff678efb5e61e00979e47b77b3
Johnneeee <johnnynguyyen@gmail.com>
2026-03-15 17:44:20 +0100

    port fix

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const pool = require("./db")
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
@@ -66,6 +66,6 @@ app.post("/songsRequested/newSong", async (req, res) => {
 });
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("Server has started on port 5000")
 });
