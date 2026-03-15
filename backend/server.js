e10327e9c8af2e1948c77bb84bc6101198d489b3
Johnneeee <johnnynguyyen@gmail.com>
2026-03-15 18:15:07 +0100

    5000

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const pool = require("./db")
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
