0691edc99be87928b332707a321fc7b2fb089b40
Johnneeee <johnnynguyyen@gmail.com>
2026-03-15 18:04:14 +0100

    status check

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const pool = require("./db")
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // middleware
 app.use(cors())
